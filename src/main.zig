58ce458c9d28760fcf5ecb83caee1fe39c1e8deb
Josephamumford <josephamumford@gmail.com>
2023-10-11 21:12:59 -0500

    add html

--- src/main.zig
+++ src/main.zig
@@ -26,6 +26,8 @@ fn runServer(server: *http.Server, allocator: std.mem.Allocator) !void {
 }
 
 fn handleRequest(response: *http.Server.Response, allocator: std.mem.Allocator) !void {
+    var timer = try std.time.Timer.start();
+
     log.info("{s} {s} {s}", .{ @tagName(response.request.method), @tagName(response.request.version), response.request.target });
 
     const body = try response.reader().readAllAlloc(allocator, 8192);
@@ -44,7 +46,12 @@ fn handleRequest(response: *http.Server.Response, allocator: std.mem.Allocator) 
         try sendResponse(response, response_message);
     } else if (std.mem.eql(u8, response.request.target, "/route2")) {
         response_message = routeTwo(response);
-        try response.headers.append("content-type", HeaderContentType.TextPlain.toString());
+        try response.headers.append("content-type", HeaderContentType.JSON.toString());
+
+        try sendResponse(response, response_message);
+    } else if (std.mem.eql(u8, response.request.target, "/route3")) {
+        response_message = routeThree(response);
+        try response.headers.append("content-type", HeaderContentType.TextHtml.toString());
 
         try sendResponse(response, response_message);
     } else if (std.mem.startsWith(u8, response.request.target, "/get")) {
@@ -56,6 +63,11 @@ fn handleRequest(response: *http.Server.Response, allocator: std.mem.Allocator) 
         response.status = .not_found;
         try sendResponse(response, "Resource not found");
     }
+
+    const time_end_ns = timer.read();
+    const time_end_us: u64 = time_end_ns / 1000;
+    const time_end_ms = @as(f64, @floatFromInt(time_end_us)) / 1000.0;
+    std.log.info("{}ns : {}us : {}ms", .{ time_end_ns, time_end_us, time_end_ms });
 }
 
 fn sendResponse(response: *http.Server.Response, data: []const u8) anyerror!void {
@@ -72,7 +84,13 @@ fn routeOne(response: *http.Server.Response) []const u8 {
 
 fn routeTwo(response: *http.Server.Response) []const u8 {
     response.status = .ok;
-    return "User logged in";
+    return "{\"message\":\"User logged in\"}";
+}
+
+fn routeThree(response: *http.Server.Response) []const u8 {
+    response.status = .ok;
+
+    return "<!DOCTYPE html><html><body><h1>Route 3</h1><p>Test HTML Template</p></body></html>";
 }
 
 pub fn main() !void {
